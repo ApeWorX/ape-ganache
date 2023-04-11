2126162e018ae7f613c0868381358e9037a416ec
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2023-04-10 20:47:13 -0400

    chore: add avax and bsc networks

--- ape_ganache/__init__.py
+++ ape_ganache/__init__.py
@@ -23,21 +23,29 @@ def providers():
     for network in NETWORKS:
         yield "ethereum", f"{network}-fork", GanacheForkProvider
 
-    yield "fantom", LOCAL_NETWORK_NAME, GanacheProvider
-    yield "fantom", "opera-fork", GanacheForkProvider
-    yield "fantom", "testnet-fork", GanacheForkProvider
-
     yield "arbitrum", LOCAL_NETWORK_NAME, GanacheProvider
     yield "arbitrum", "mainnet-fork", GanacheForkProvider
     yield "arbitrum", "goerli-fork", GanacheForkProvider
 
-    yield "polygon", LOCAL_NETWORK_NAME, GanacheProvider
-    yield "polygon", "mainnet-fork", GanacheForkProvider
-    yield "polygon", "mumbai-fork", GanacheForkProvider
+    yield "avalanche", LOCAL_NETWORK_NAME, GanacheProvider
+    yield "avalanche", "mainnet-fork", GanacheForkProvider
+    yield "avalanche", "fuji-fork", GanacheForkProvider
+
+    yield "bsc", LOCAL_NETWORK_NAME, GanacheProvider
+    yield "bsc", "mainnet-fork", GanacheForkProvider
+    yield "bsc", "testnet-fork", GanacheForkProvider
+
+    yield "fantom", LOCAL_NETWORK_NAME, GanacheProvider
+    yield "fantom", "opera-fork", GanacheForkProvider
+    yield "fantom", "testnet-fork", GanacheForkProvider
 
     yield "optimism", LOCAL_NETWORK_NAME, GanacheProvider
     yield "optimism", "mainnet-fork", GanacheForkProvider
     yield "optimism", "goerli-fork", GanacheForkProvider
+
+    yield "polygon", LOCAL_NETWORK_NAME, GanacheProvider
+    yield "polygon", "mainnet-fork", GanacheForkProvider
+    yield "polygon", "mumbai-fork", GanacheForkProvider
 
 
 __all__ = [
